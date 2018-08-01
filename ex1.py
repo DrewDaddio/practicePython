62d4c59ca1a882b429484b7e22654c5ed2b4d823
Drew Daddio <DrewDaddio@users.noreply.github.com>
2018-07-31 21:56:58 -0700

    Add files via upload (#1)
    Example 1 updated to actual do the exercise

--- ex1.py
+++ ex1.py
@@ -1,6 +1,7 @@
 # Exercise 1: Character Input
 
-print("The challenge is to create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.")
+print("The challenge is to create a program that asks the user to enter their name and their age.")
+print("Print out a message addressed to them that tells them the year that they will turn 100 years old.")
 
 print("Hey you!")
 name = input("What is your name?: ")
@@ -9,3 +10,9 @@ print("Your name is " + name)
 print("Hey " + name)
 age = input("What is your age?: ")
 print("Your age is: " + age)
+
+ageInt = int(age)
+print("In 100 years you'll be " + str(int(100 + ageInt)) + " years old!")
+
+year = str((2018 - ageInt) + 100)
+print(name + " , you will turn 100 in the year " + year)
