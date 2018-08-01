e48320480b207d08d1f6d43c8a09e1be664fc75c
Drew Daddio <DrewDaddio@users.noreply.github.com>
2018-07-31 21:35:45 -0700

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,11 @@
+# Exercise 1: Character Input
+
+print("The challenge is to create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the year that they will turn 100 years old.")
+
+print("Hey you!")
+name = input("What is your name?: ")
+print("Your name is " + name)
+
+print("Hey " + name)
+age = input("What is your age?: ")
+print("Your age is: " + age)
